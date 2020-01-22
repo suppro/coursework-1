5100849d77610946aa22a5aecc5fb4b2eaaef74c
dftusert <dftusert@gmail.com>
2020-01-22 21:06:12 +0300

    правки в dev

--- Raskatova1/Program.cs
+++ Raskatova1/Program.cs
@@ -11,41 +11,47 @@ namespace Raskatova1
     {
         private static Database DB;
 
-        public static List<Result> NewResult(int numberOfRunners, Random rnd, string fileName)
+        public static string FullName(List<string> namesStorage)
         {
-            int age; int start; int finish; int total; string rank;
+            Random rnd = new Random();
+            int skip = rnd.Next(0, namesStorage.Count());
+            string line = namesStorage.ElementAt(skip);
+            namesStorage.RemoveAt(skip);
+            return line;
+        }
+
+        public static List<Result> NewResult(int numberOfRunners, string fileName)
+        {
+            int age, start, finish, total;
+            string rank;
+
+            Random rnd = new Random();
             List<string> file = File.ReadLines(fileName).ToList();
             List<Result> listResult = new List<Result>();
+            
             for (int i = 0; i < numberOfRunners; ++i)
             {
                 age = rnd.Next(18, 80);
-                rank = AtleteRank(age, rnd);
+                rank = AtleteRank(age);
                 start = rnd.Next(0, 180);
                 total = FinishTime(rank, age, rnd);
                 finish = total + start;
-                listResult.Add(new Result() { FullName = FullName(file, rnd), Age = age, Rank = rank, TimeOfStart = start, TimeOfFinish = finish, TotalTime = total });
+                listResult.Add(new Result() { FullName = FullName(file), Age = age, Rank = rank, TimeOfStart = start, TimeOfFinish = finish, TotalTime = total });
             }
             listResult.Sort((Result x, Result y) => { return x.TotalTime.CompareTo(y.TotalTime); });
             return listResult;
         }
-        public static string FullName(List<string> file, Random rnd)
+
+        public static void AgeResult(List<Result> listResult, int ageMin, int ageMax)
         {
-            int count = file.Count();
-            int skip = rnd.Next(0, count);
-            string line = file.ElementAt(skip);
-            file.RemoveAt(skip);
-            return line;
-        }
-        public static void AgeResult(List<Result> listResult, int ageMin, int ageMax) //сделать вывод из таблицы тут
-        {
-            int timeWiner = 0;
+            int timeWiner = -1;
             int ageWinner = 0;
             string nameWiner = "";
             bool flag = false;
 
             for (int i = 0; i < listResult.Count; i++)
             {
-                if (listResult[i].Age >= ageMin && listResult[i].Age <= ageMax && (listResult[i].TimeOfFinish - listResult[i].TimeOfStart) < timeWiner)
+                if (listResult[i].Age >= ageMin && listResult[i].Age <= ageMax && ((listResult[i].TimeOfFinish - listResult[i].TimeOfStart) < timeWiner || timeWiner == -1))
                 {
                     timeWiner = listResult[i].TotalTime;
                     nameWiner = listResult[i].FullName;
@@ -53,6 +59,7 @@ namespace Raskatova1
                     flag = true;
                 }
             }
+
             TimeSpan ts = TimeSpan.FromSeconds(timeWiner);
             if(flag) Console.WriteLine("В возрастной группе " + ageMin + " - " + ageMax + " лет побеждает " + nameWiner + " (" + ageWinner + " лет) с результатом " + "{0}:{1}:{2}", ts.Hours, ts.Minutes, ts.Seconds);
             else Console.WriteLine("В возрастной группе " + ageMin + " - " + ageMax + " лет нет атлетов");
@@ -62,26 +69,36 @@ namespace Raskatova1
         {
             int finish;
             int addTime = age * 2;
-            if (rank == "Пожилой") { finish = rnd.Next(5000 + addTime, 8000 + addTime); }
-            else if (rank == "Начинающий") { finish = rnd.Next(3601, 7600); }
-            else if (rank == "Любитель") { finish = rnd.Next(2341, 3301); }
-            else if (rank == "Атлет") { finish = rnd.Next(1951, 2340); }
-            else if (rank == "Элита") { finish = rnd.Next(1740, 1950); }
-            else { throw new Exception("Ранг не определен"); }
+            switch(rank)
+            {
+                case "Пожилой":
+                    finish = rnd.Next(5000 + addTime, 8000 + addTime); break;
+                case "Начинающий":
+                    finish = rnd.Next(3601, 7600); break;
+                case "Любитель":
+                    finish = rnd.Next(2341, 3301); break;
+                case "Атлет":
+                    finish = rnd.Next(1951, 2340); break;
+                case "Элита":
+                    finish = rnd.Next(1740, 1950); break;
+                default:
+                    throw new Exception("Ранг не определен");
+            }
             return finish;
         }
-        public static string AtleteRank(int age, Random rnd)
+
+        public static string AtleteRank(int age)
         {
-            string rank = "";
+            string rank;
             double addChance = 0.03 + (age - 45) * 0.03;
+
+            Random rnd = new Random();
             double setRank = rnd.NextDouble();
             if (age >= 60) { rank = "Пожилой"; }
             else
             {
-                if (age >= 45)
-                {
-                    setRank = rnd.NextDouble() + addChance;
-                }
+                if (age >= 45) setRank += addChance;
+
                 if (setRank <= 0.02) { rank = "Элита"; }
                 else if (setRank > 0.02 && setRank <= 0.15) { rank = "Атлет"; }
                 else if (setRank > 0.15 && setRank <= 0.5) { rank = "Любитель"; }
@@ -90,7 +107,7 @@ namespace Raskatova1
             }
             return rank;
         }
-        public static void SortResult(List<Result> listResult, int ageMin, int ageMax, bool subMenu, int numberOfRunners) //сделать вывод из таблицы тут
+        public static void SortResult(List<Result> listResult, int ageMin, int ageMax, bool subMenu)
         {
             TimeSpan tsTotal, tsFinish, tsStart;
             var allResultInAge = from age in listResult
@@ -110,9 +127,10 @@ namespace Raskatova1
                     tsTotal.Hours, tsTotal.Minutes, tsTotal.Seconds);
             }
 
-            CheckSubMenu(listResult, subMenu, numberOfRunners);
+            CheckSubMenu(listResult, subMenu);
         }
-        public static void CheckSubMenu(List<Result> listResult, bool subMenu, int numberOfRunners)
+
+        public static void CheckSubMenu(List<Result> listResult, bool subMenu)
         {
             string answer;
             if (subMenu)
@@ -123,13 +141,13 @@ namespace Raskatova1
                 switch(answer)
                 {
                     case "1":
-                        Menu(listResult, true, numberOfRunners); break;
+                        Menu(listResult, true); break;
                     case "2":
-                        Menu(listResult, false, numberOfRunners); break;
+                        Menu(listResult, false); break;
                     default:
                         Console.WriteLine("Ошибка ввода: данного варианта ответа не существует. Возврат в подменю...");
                         Console.ReadKey();
-                        Menu(listResult, true, numberOfRunners);
+                        Menu(listResult, true);
                         break;
                 }
             }
@@ -137,20 +155,19 @@ namespace Raskatova1
             {
                 Console.WriteLine("Для возврата на главную нажмите на любую клавишу.");
                 Console.ReadKey();
-                Menu(listResult, false, numberOfRunners);
+                Menu(listResult, false);
             }
         }
-        public static void Menu(List<Result> listResult, bool subMenu, int numberOfRunners)
+        public static void Menu(List<Result> listResult, bool subMenu)
         {
             string answer;
-
             while (true)
             {
                 if (!subMenu)
                 {
                     Console.Clear();
                     Console.WriteLine("Забег на 10 км.");
-                    Console.WriteLine("Всего участников забега - " + numberOfRunners);
+                    Console.WriteLine("Всего участников забега - " + listResult.Count);
                     Console.WriteLine("Для вывода результатов в возрастных группах введите 1");
                     Console.WriteLine("Для вывода результатов забега введите 2");
                     Console.WriteLine("Для вывода результатов за всю историю соревнований 3");
@@ -178,35 +195,35 @@ namespace Raskatova1
                         {
                             case "0":
                                 subMenu = false;
-                                Menu(listResult, subMenu, numberOfRunners);
+                                Menu(listResult, subMenu);
                                 break;
                             case "1":
-                                SortResult(listResult, 18, 30, true, numberOfRunners); break;
+                                SortResult(listResult, 18, 30, true); break;
                             case "2":
-                                SortResult(listResult, 31, 40, true, numberOfRunners); break;
+                                SortResult(listResult, 31, 40, true); break;
                             case "3":
-                                SortResult(listResult, 41, 50, true, numberOfRunners); break;
+                                SortResult(listResult, 41, 50, true); break;
                             case "4":
-                                SortResult(listResult, 61, 70, true, numberOfRunners); break;
+                                SortResult(listResult, 61, 70, true); break;
                             case "5":
-                                SortResult(listResult, 71, 85, true, numberOfRunners); break;
+                                SortResult(listResult, 71, 85, true); break;
                             default:
                                 Console.WriteLine("Ошибка ввода: данного варианта ответа не существует. Возврат в подменю...");
                                 Console.ReadKey();
-                                Menu(listResult, true, numberOfRunners);
+                                Menu(listResult, true);
                                 break;
                         }
                         break;
 
                     case "2":
                         Console.WriteLine("Результаты:");
-                        SortResult(listResult, 18, 80, false, numberOfRunners);
+                        SortResult(listResult, 18, 80, false);
                         break;
 
                     case "3":
                         Console.WriteLine("Результаты атлетов за всю историю соревнований:");
                         DB.PrintTable();
-                        CheckSubMenu(listResult, false, numberOfRunners);
+                        CheckSubMenu(listResult, false);
                         Console.ReadKey();
                         break;
                     default:
@@ -240,14 +257,14 @@ namespace Raskatova1
                 Random rnd = new Random();
                 int numberOfRunners = rnd.Next(5, 10);
 
-                List<Result> listResult = NewResult(numberOfRunners, rnd, fileName);
+                List<Result> listResult = NewResult(numberOfRunners, fileName);
 
                 DB.LoadResults(listResult);
 
                 Console.WriteLine("Результаты загружены в базу данных. Запуск меню...");
                 Console.ReadKey();
 
-                Menu(listResult, false, numberOfRunners);
+                Menu(listResult, false);
             }
             catch (SQLiteException ex)
             {
